fe25d3bd4c97138a14ad57058e83f36dc0d1c6ff
Styles-Theandriod <emmanuelajah190@gmail.com>
2025-12-05 16:42:41 +0100

    Working on TermsOfService page which is the last page

--- app/TermsOfService.tsx
+++ app/TermsOfService.tsx
@@ -1,13 +1,13 @@
 // TermsOfServiceScreen.js
 import React from "react";
-import { ScrollView, StyleSheet, Text, Linking, TouchableOpacity } from "react-native";
+import { Linking, ScrollView, StyleSheet, Text } from "react-native";
 
 const TermsOfService = () => {
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.title}>Terms of Service</Text>
+      <Text style={styles.title}>TERMS OF SERVICES</Text>
 
-      <Text style={styles.sectionTitle}>1. Platform Description</Text>
+      <Text style={styles.sectionTitle}>1.1 Platform Description</Text>
       <Text style={styles.text}>
         GO Pickup is a web and mobile application-based platform accessible at{" "}
         <Text
@@ -29,7 +29,7 @@ const TermsOfService = () => {
         {"\n"}These services are collectively referred to as the “Services.”
       </Text>
 
-      <Text style={styles.sectionTitle}>2. Operator</Text>
+      <Text style={styles.sectionTitle}>1.2 OPERATOR</Text>
       <Text style={styles.text}>
         The platform is operated by GO Pickup Logistics Ltd. (“GO Pickup”).
         Access to and use of the platform is governed by these General Terms and
@@ -38,13 +38,36 @@ const TermsOfService = () => {
         you disagree, you must discontinue use immediately.
       </Text>
 
-      <Text style={styles.sectionTitle}>3. Account Registration</Text>
+      <Text style={styles.sectionTitle}>1.3 ADDITIONAL TERMS</Text>
       <Text style={styles.text}>
-        To register an account, you must provide accurate information including:
-        full name, phone number, email address, location, username, password,
-        and Nigerian Identity Number (NIN).
-        {"\n\n"}For companies: CAC Registration Number, vehicle type/model, and
-        a valid driver’s license.
+        Some Features may be governed by additional terms ("Additional Terms"). 
+        in the event of a conflict between General and Additional Terms, 
+        the Additional Terms will prevail.
+      </Text>
+
+      <Text style={styles.sectionTitle}>1.4 CHANGES TO TERMS</Text>
+      <Text style={styles.text}>
+        Go Pickup reserves the right to amend these General and Additional Terms. 
+        Continued use after such changes constistute acceptence
+      </Text>
+
+      <Text style={styles.sectionTitle}>1.5 PRIVACY</Text>
+      <Text style={styles.text}>
+        Personal data is handled per the Go Pickup Privacy Policy.
+      </Text>
+
+      <Text style={styles.sectionTitles}>2. ACCOUNT REGISTRATION</Text>
+
+      <Text style={styles.sectionTitle}>2.1 Required Information</Text>
+      <Text style={styles.text}>
+        To register an account ("Account"), you must provide: 
+      </Text>
+
+      <Text style={styles.texts}>
+        {"\n"}• full name, phone number, email address, location.
+        {"\n"}• username, password, and Nigerian Identity Number (NIN).
+        {"\n"}• For companies: CAC Registration Number, vehicle type/model.
+        {"\n"}• valid driver’s license.
       </Text>
       <Text style={styles.text}>
         By registering, you agree that all information is true and will be kept
@@ -52,14 +75,51 @@ const TermsOfService = () => {
         phone, or in-app notifications related to your service use.
       </Text>
 
-      <Text style={styles.sectionTitle}>4. Relationship</Text>
+      <Text style={styles.sectionTitle}>2.2 REPRESENTATION AND UPDATES</Text>
       <Text style={styles.text}>
-        GO Pickup acts only as a facilitator between Senders and Pilots. Pilots
-        are independent contractors and not employees. GO Pickup does not
-        directly provide delivery services.
+        You agree that the information provided is accurate and will be updated as neccessary.
+      </Text>
+      <Text style={styles.sectionTitle}>2.3 Communication Consent</Text>
+      <Text style={styles.text}>
+        You consent to recieve communication via SMS, email, phone, or in-app notifications related service use.
+      </Text>
+      <Text style={styles.sectionTitle}>2.4 ELIGIBILITY</Text>
+      <Text style={styles.text}>
+        You may not use the platform if you:
+      </Text>
+      <Text style={styles.text}>
+        {"\n"}• Are under legal contracting age.
+        {"\n"}• Are legally restricted from using the service.
       </Text>
 
-      <Text style={styles.sectionTitle}>5. Pilot Obligations</Text>
+      <Text style={styles.sectionTitle}>2.5 USER WARRANTIES</Text>
+      <Text style={styles.text}>
+        By registering, you warrent that:
+      </Text>
+      <Text style={styles.text}>
+        {"\n"}• You meet the legal age requirement.
+        {"\n"}• You're authorized to act on behalf of your business (if applicable).
+        {"\n"}• You possess the skills, licenses, and eequipment to perform service.
+        {"\n"}• You comply with Nigerian law and GO Pickup standards.
+      </Text>
+
+      <Text style={styles.sectionTitle}>3. RELATIONSHIP</Text>
+      <Text style={styles.text}>
+       {"\n"}• GO Pickup is a facilitator and not a logistics provider.  
+       {"\n"}• Pilots are independent contractors and not employees. 
+       {"\n"}• GO Pickup does not directly provide delivery services.
+      </Text>
+
+      <Text style={styles.sectionTitles}>4. PERFORMANCE OF SERVICES</Text>
+      <Text style={styles.sectionTitle}>Pilots must:</Text>
+      <Text style={styles.text}>
+       {"\n"}• Deliver services lawfully, proffessionally, and diligently.  
+       {"\n"}• Follow road safety and product-specific delivery rules. 
+       {"\n"}• Enable GPS tracking during deliveries.
+       {"\n"}• Notify GO Pickup of delays, incidents, or dispute stage.
+      </Text>
+
+      <Text style={styles.sectionTitle}>5. PILOT OBLIGATIONS</Text>
       <Text style={styles.text}>
         Pilots must:
         {"\n"}• Maintain account confidentiality.
@@ -69,21 +129,37 @@ const TermsOfService = () => {
         {"\n"}• Not reverse-engineer or exploit the platform.
       </Text>
 
-      <Text style={styles.sectionTitle}>6. Insurance and Risk</Text>
+      <Text style={styles.sectionTitle}>6. INSURANCE </Text>
       <Text style={styles.text}>
-        Pilots are required to maintain valid vehicle and third-party insurance,
-        workers’ compensation (if applicable), and any other coverages as
-        required by law. Pilots bear all risks associated with service delivery.
+        Pilots must maintain:
+        {"\n"}• Vehicle and third-party insurance.
+        {"\n"}• Workers’ compensation (if applicable).
+        {"\n"}• Other coverages as required by law.
       </Text>
 
-      <Text style={styles.sectionTitle}>7. Payments</Text>
+      <Text style={styles.sectionTitle}>7. RISK</Text>
       <Text style={styles.text}>
-        GO Pickup acts as an agent to collect payments from Senders. Payouts to
-        Pilots are processed within 48 hours of job completion. Tax invoices are
-        issued by GO Pickup on behalf of the Pilot.
+        Pilot bear all risks associated with service delivery.
       </Text>
 
-      <Text style={styles.sectionTitle}>8. Reviews and Ratings</Text>
+      <Text style={styles.sectionTitle}>8. VERIFICATION</Text>
+      <Text style={styles.text}>
+        GO Pickup may verify your identity, vehicle registration and other submitted information.
+      </Text>
+      
+      <Text style={styles.sectionTitle}>9. EQUIPMENT & EXPENSES</Text>
+      <Text style={styles.text}>
+        Pilots must supply and maintain thier own vehicle and tools.
+      </Text>
+
+      <Text style={styles.sectionTitle}>10. INVOICING & PAYMENTS</Text>
+      <Text style={styles.text}>
+        {"\n"}• GO Pickup acts as an agent to collect payments from Senders.
+        {"\n"}• Pilots are processed within 48 hours of job completion.
+        {"\n"}• Tax invoices are issued by GO Pickup on behalf of the Pilot.
+      </Text>
+
+      <Text style={styles.sectionTitle}>8. REVIEWS AND RATING</Text>
       <Text style={styles.text}>
         Senders may rate Pilots as Good, Neutral, or Bad. These reviews affect a
         Pilot’s visibility and credibility on the platform. GO Pickup reserves
@@ -152,7 +228,7 @@ const TermsOfService = () => {
         </Text>
       </Text>
 
-      <Text style={styles.text}>Last updated: November 2025</Text>
+      
     </ScrollView>
   );
 };
@@ -174,11 +250,22 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     marginTop: 20,
   },
+  sectionTitles: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 20,
+  },
   text: {
     fontSize: 15,
     lineHeight: 22,
     color: "#444",
     marginTop: 8,
+  },
+  texts: {
+    fontSize: 15,
+    lineHeight: 22,
+    color: "#444",
+    marginTop: 0,
   },
   link: {
     color: "#007BFF",
